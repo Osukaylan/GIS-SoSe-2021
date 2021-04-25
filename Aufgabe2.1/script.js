e07724e81b012cee3028af7c3ec74ae2ad64eaa0
Osukaylan <erdem.aktas@hs-furtwangen.de>
2021-04-26 00:01:49 +0200

    New task

---
+++ Aufgabe2.1/script.js
@@ -0,0 +1,12 @@
+"use strict";
+function a1() {
+    let x = "Alles Gute!";
+    console.log(x);
+    func1();
+    console.log("Alles Logo!");
+}
+a1();
+function func1() {
+    console.log("Alles klar?");
+}
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe2.1/script.ts
@@ -0,0 +1,12 @@
+function a1(): void {
+    let x: string = "Alles Gute!";
+    console.log(x);
+    func1();
+    console.log("Alles Logo!");
+}
+
+a1();
+
+function func1(): void {
+    console.log("Alles klar?");
+}
